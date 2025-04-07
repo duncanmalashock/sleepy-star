3dadddd4164f0b373d3cb7d632bf539bcb0388f4
Duncan Malashock <duncanmalashock@gmail.com>
2025-04-07 00:26:03 -0400

    Force astro to optimize paper

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import netlify from '@astrojs/netlify';
 
-// https://astro.build/config
 export default defineConfig({
-  adapter: netlify()
+  adapter: netlify(),
+  vite: {
+    optimizeDeps: {
+      include: ['paper']
+    }
+  }
 });
